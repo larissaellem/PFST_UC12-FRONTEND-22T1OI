a9e08206234a39f994f09459cf5765f95ec49b08
Larissa Ellen <larissaellen415@gmail.com>
2022-08-12 23:23:38 -0300

    finalizando

--- SA3/src/app/views/header/header.component.ts
+++ SA3/src/app/views/header/header.component.ts
@@ -1,26 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { LoginModal } from "src/app/components/login/login.component";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: "./header.component.html"
+  selector: "app-header",
+  templateUrl: "./header.component.html",
 })
-export class HeaderComponent {
+export class HeaderComponent extends LoginModal implements OnInit {
+  ngOnInit() {
+    const user = localStorage.getItem("user");
+    if (user) {
+      const userData = JSON.parse(user);
+      (
+        document.querySelector("#header-login-name") as HTMLElement
+      ).textContent = "Olá, " + userData.username;
+    }
+  }
+
   links = [
     {
       name: "Home",
-      url: "#"
+      url: "#",
     },
     {
       name: "Produtos",
-      url: "#"
+      url: "#",
     },
     {
       name: "Monte seu PC",
-      url: "#"
+      url: "#",
     },
     {
       name: "OFERTAS",
-      url: "#"
+      url: "#",
     },
   ];
 
--- SA3/src/app/views/home/home.component.ts
+++ SA3/src/app/views/home/home.component.ts
@@ -15,49 +15,4 @@ export class HomeComponent implements OnInit {
       left: direction === "left" ? -420 : 420,
     });
   }
-
-  // funcao de login
-  // login(form: HTMLFormElement) {
-  //   const email = form.querySelector<HTMLInputElement>("input[type=email]")?.value;
-  //   const password = form.querySelector<HTMLInputElement>("input[type=password]")?.value;
-  //   if(!email || !password) return alert("Preencha todos os campos para fazer login");
-
-  //   const username = email.split("@")[0];
-
-  //   localStorage.setItem("user", JSON.stringify({ email, password, username }));
-  //   document.querySelector("#header-login-name").textContent = "Olá, " + username;
-
-  //   $("#loginModal").modal("hide");
-  // }
-
-  // // funcao de logout
-  // logout() {
-  //   localStorage.removeItem("user");
-  //   window.location.reload();
-  // }
-
-  // // acionamento do modal
-  // triggerModal() {
-  //   const user = localStorage.getItem("user");
-  //   if (!user) return $("#loginModal").modal("show");
-
-  //   const userData = JSON.parse(user);
-  //   updateUsername(userData.username);
-  //   $("#userModal").modal("show");
-  // }
-
-  // // atualizar referencias de usuario
-  // updateUsername(username) {
-  //   document.querySelector("#header-login-name").textContent = "Olá, " + username;
-  //   document.querySelectorAll("#usernameContent").forEach((element) => {
-  //     element.textContent = username;
-  //   });
-  // }
-
-  // // atribuindo usuario logado
-  // const user = localStorage.getItem("user");
-  // if (user) {
-  //   const userData = JSON.parse(user);
-  //   updateUsername(userData.username);
-  // }
 }
